7017485e4ead6b5515a196ce5ffc761e85946db4
Prashant Garg <58599470+pekka-dev@users.noreply.github.com>
2021-04-25 23:38:12 +0530

    Complete FAQ.jsx component

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,12 +1,22 @@
-import { makeStyles } from '@material-ui/core'
-import Grid           from '@material-ui/core/Grid'
-import Container      from '@material-ui/core/Container'
-import Typography     from '@material-ui/core/Typography'
-import IconButton     from '@material-ui/core/IconButton'
-import SearchIcon     from '@material-ui/icons/Search'
-import Autocomplete   from '@material-ui/lab/Autocomplete'
-import TextField      from '@material-ui/core/TextField'
-import React          from 'react'
+import Box               from '@material-ui/core/Box'
+import { makeStyles }    from '@material-ui/core'
+import Grid              from '@material-ui/core/Grid'
+import Container         from '@material-ui/core/Container'
+import Typography        from '@material-ui/core/Typography'
+import IconButton        from '@material-ui/core/IconButton'
+import SearchIcon        from '@material-ui/icons/Search'
+import Autocomplete      from '@material-ui/lab/Autocomplete'
+import TextField         from '@material-ui/core/TextField'
+import Button            from '@material-ui/core/Button'
+import Dialog            from '@material-ui/core/Dialog'
+import DialogActions     from '@material-ui/core/DialogActions'
+import DialogContent     from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle       from '@material-ui/core/DialogTitle'
+import Card              from '@material-ui/core/Card'
+import CardContent       from '@material-ui/core/CardContent'
+import { useState }      from 'react'
+import React, { useRef } from 'react'
 
 const useStyles = makeStyles((theme) => ({
     root      : {
@@ -25,14 +35,39 @@ const useStyles = makeStyles((theme) => ({
     iconButton: {
         padding: 10,
     },
-    divider   : {
-        height: 28,
-        margin: 4,
+    faq       : {
+        minWidth: 275,
+    },
+    bullet    : {
+        display  : 'inline-block',
+        margin   : '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title     : {
+        fontSize: 14,
+    },
+    pos       : {
+        marginBottom: 12,
     },
 }))
 
 export default function FAQ () {
     const classes = useStyles()
+    const searchRef = useRef()
+    const [open, setOpen] = useState(false)
+    const [que, setQue] = useState()
+
+    const handleClose = () => {
+        setQue()
+        setOpen(false)
+    }
+
+    const handleAutocompleteSelect = (event, value) => {
+        console.log(value)
+        setQue(value)
+        setOpen(true)
+    }
+
     return (<Container>
         <Grid container>
             <Grid item xs={12} container justify="center">
@@ -42,12 +77,13 @@ export default function FAQ () {
             </Grid>
             <Grid item xs={12} container justify="center">
                 <Autocomplete
-                    id="combo-box-demo"
-                    options={top100Films}
-                    getOptionLabel={(option) => option.title}
-                    style={{ width: 300 }}
+                    onChange={handleAutocompleteSelect}
+                    options={ques}
+                    getOptionLabel={(option) => option.que}
+                    className={classes.search}
                     renderInput={(params) => <TextField {...params}
-                                                        label="Combo box"
+                                                        label="Frequently asked questions..."
+                                                        inputRef={searchRef}
                                                         InputProps={{
                                                             ...params.InputProps,
                                                             endAdornment: (
@@ -63,309 +99,96 @@ export default function FAQ () {
                                                         }}/>}
                 />
             </Grid>
+            <Grid item xs={12} container justify="center" component={Box}
+                  mt={1}>
+                {ques.map(q => q.id < 3 &&
+                               (<Grid key={q.id} item xs={12} sm={6} container
+                                      justify="center" component={Box} p={3}>
+                                   <Card className={classes.faq}
+                                         variant="outlined">
+                                       <CardContent>
+                                           <Typography className={classes.title}
+                                                       color="textSecondary"
+                                                       gutterBottom>
+                                               {q.ans[0].from}
+                                           </Typography>
+                                           <Typography variant="h5"
+                                                       component="h2">
+                                               {q.que}
+                                           </Typography>
+                                           <Typography variant="body2"
+                                                       component="p">
+                                               {q.ans[0].replay}
+                                           </Typography>
+                                       </CardContent>
+                                   </Card>
+                               </Grid>))}
+            </Grid>
         </Grid>
+        {que ? <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <DialogTitle>{que.que}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {que.ans[0].replay}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog> : null}
     </Container>)
 }
 
-const top100Films = [
+const ques = [
     {
-        title: 'The Shawshank Redemption',
-        year : 1994,
-    }, {
-        title: 'The Godfather',
-        year : 1972,
-    }, {
-        title: 'The Godfather: Part II',
-        year : 1974,
-    }, {
-        title: 'The Dark Knight',
-        year : 2008,
-    }, {
-        title: '12 Angry Men',
-        year : 1957,
-    }, {
-        title: 'Schindler\'s List',
-        year : 1993,
-    }, {
-        title: 'Pulp Fiction',
-        year : 1994,
-    }, {
-        title: 'The Lord of the Rings: The Return of the King',
-        year : 2003,
-    }, {
-        title: 'The Good, the Bad and the Ugly',
-        year : 1966,
-    }, {
-        title: 'Fight Club',
-        year : 1999,
-    }, {
-        title: 'The Lord of the Rings: The Fellowship of the Ring',
-        year : 2001,
-    }, {
-        title: 'Star Wars: Episode V - The Empire Strikes Back',
-        year : 1980,
-    }, {
-        title: 'Forrest Gump',
-        year : 1994,
-    }, {
-        title: 'Inception',
-        year : 2010,
-    }, {
-        title: 'The Lord of the Rings: The Two Towers',
-        year : 2002,
-    }, {
-        title: 'One Flew Over the Cuckoo\'s Nest',
-        year : 1975,
-    }, {
-        title: 'Goodfellas',
-        year : 1990,
-    }, {
-        title: 'The Matrix',
-        year : 1999,
-    }, {
-        title: 'Seven Samurai',
-        year : 1954,
-    }, {
-        title: 'Star Wars: Episode IV - A New Hope',
-        year : 1977,
-    }, {
-        title: 'City of God',
-        year : 2002,
-    }, {
-        title: 'Se7en',
-        year : 1995,
-    }, {
-        title: 'The Silence of the Lambs',
-        year : 1991,
-    }, {
-        title: 'It\'s a Wonderful Life',
-        year : 1946,
-    }, {
-        title: 'Life Is Beautiful',
-        year : 1997,
-    }, {
-        title: 'The Usual Suspects',
-        year : 1995,
-    }, {
-        title: 'Léon: The Professional',
-        year : 1994,
-    }, {
-        title: 'Spirited Away',
-        year : 2001,
-    }, {
-        title: 'Saving Private Ryan',
-        year : 1998,
-    }, {
-        title: 'Once Upon a Time in the West',
-        year : 1968,
-    }, {
-        title: 'American History X',
-        year : 1998,
-    }, {
-        title: 'Interstellar',
-        year : 2014,
-    }, {
-        title: 'Casablanca',
-        year : 1942,
-    }, {
-        title: 'City Lights',
-        year : 1931,
-    }, {
-        title: 'Psycho',
-        year : 1960,
-    }, {
-        title: 'The Green Mile',
-        year : 1999,
-    }, {
-        title: 'The Intouchables',
-        year : 2011,
-    }, {
-        title: 'Modern Times',
-        year : 1936,
-    }, {
-        title: 'Raiders of the Lost Ark',
-        year : 1981,
-    }, {
-        title: 'Rear Window',
-        year : 1954,
-    }, {
-        title: 'The Pianist',
-        year : 2002,
-    }, {
-        title: 'The Departed',
-        year : 2006,
-    }, {
-        title: 'Terminator 2: Judgment Day',
-        year : 1991,
-    }, {
-        title: 'Back to the Future',
-        year : 1985,
-    }, {
-        title: 'Whiplash',
-        year : 2014,
-    }, {
-        title: 'Gladiator',
-        year : 2000,
-    }, {
-        title: 'Memento',
-        year : 2000,
-    }, {
-        title: 'The Prestige',
-        year : 2006,
-    }, {
-        title: 'The Lion King',
-        year : 1994,
-    }, {
-        title: 'Apocalypse Now',
-        year : 1979,
-    }, {
-        title: 'Alien',
-        year : 1979,
-    }, {
-        title: 'Sunset Boulevard',
-        year : 1950,
-    }, {
-        title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-        year : 1964,
-    }, {
-        title: 'The Great Dictator',
-        year : 1940,
-    }, {
-        title: 'Cinema Paradiso',
-        year : 1988,
-    }, {
-        title: 'The Lives of Others',
-        year : 2006,
-    }, {
-        title: 'Grave of the Fireflies',
-        year : 1988,
-    }, {
-        title: 'Paths of Glory',
-        year : 1957,
-    }, {
-        title: 'Django Unchained',
-        year : 2012,
-    }, {
-        title: 'The Shining',
-        year : 1980,
-    }, {
-        title: 'WALL·E',
-        year : 2008,
-    }, {
-        title: 'American Beauty',
-        year : 1999,
-    }, {
-        title: 'The Dark Knight Rises',
-        year : 2012,
-    }, {
-        title: 'Princess Mononoke',
-        year : 1997,
-    }, {
-        title: 'Aliens',
-        year : 1986,
-    }, {
-        title: 'Oldboy',
-        year : 2003,
-    }, {
-        title: 'Once Upon a Time in America',
-        year : 1984,
-    }, {
-        title: 'Witness for the Prosecution',
-        year : 1957,
-    }, {
-        title: 'Das Boot',
-        year : 1981,
-    }, {
-        title: 'Citizen Kane',
-        year : 1941,
-    }, {
-        title: 'North by Northwest',
-        year : 1959,
-    }, {
-        title: 'Vertigo',
-        year : 1958,
-    }, {
-        title: 'Star Wars: Episode VI - Return of the Jedi',
-        year : 1983,
-    }, {
-        title: 'Reservoir Dogs',
-        year : 1992,
-    }, {
-        title: 'Braveheart',
-        year : 1995,
-    }, {
-        title: 'M',
-        year : 1931,
-    }, {
-        title: 'Requiem for a Dream',
-        year : 2000,
-    }, {
-        title: 'Amélie',
-        year : 2001,
-    }, {
-        title: 'A Clockwork Orange',
-        year : 1971,
-    }, {
-        title: 'Like Stars on Earth',
-        year : 2007,
-    }, {
-        title: 'Taxi Driver',
-        year : 1976,
-    }, {
-        title: 'Lawrence of Arabia',
-        year : 1962,
-    }, {
-        title: 'Double Indemnity',
-        year : 1944,
-    }, {
-        title: 'Eternal Sunshine of the Spotless Mind',
-        year : 2004,
-    }, {
-        title: 'Amadeus',
-        year : 1984,
-    }, {
-        title: 'To Kill a Mockingbird',
-        year : 1962,
-    }, {
-        title: 'Toy Story 3',
-        year : 2010,
-    }, {
-        title: 'Logan',
-        year : 2017,
-    }, {
-        title: 'Full Metal Jacket',
-        year : 1987,
-    }, {
-        title: 'Dangal',
-        year : 2016,
-    }, {
-        title: 'The Sting',
-        year : 1973,
-    }, {
-        title: '2001: A Space Odyssey',
-        year : 1968,
-    }, {
-        title: 'Singin\' in the Rain',
-        year : 1952,
-    }, {
-        title: 'Toy Story',
-        year : 1995,
-    }, {
-        title: 'Bicycle Thieves',
-        year : 1948,
-    }, {
-        title: 'The Kid',
-        year : 1921,
-    }, {
-        title: 'Inglourious Basterds',
-        year : 2009,
-    }, {
-        title: 'Snatch',
-        year : 2000,
-    }, {
-        title: '3 Idiots',
-        year : 2009,
-    }, {
-        title: 'Monty Python and the Holy Grail',
-        year : 1975,
-    }]
+        id : 1,
+        que: 'How to do Lorem ipsum dolor',
+        ans: [
+            {
+                from  : 'Staff',
+                replay: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            },
+        ],
+    }, {
+        id : 2,
+        que: 'My Lorem ipsum dolor sit amet not working',
+        ans: [
+            {
+                from  : 'Staff',
+                replay: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+        ],
+    }, {
+        id : 3,
+        que: 'Lorem ipsum dolor sit amet is crashing',
+        ans: [
+            {
+                from  : 'Staff',
+                replay: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            },
+        ],
+    }, {
+        id : 4,
+        que: 'Why do I use Lorem ipsum dolor',
+        ans: [
+            {
+                from  : 'Staff',
+                replay: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            },
+        ],
+    }, {
+        id : 5,
+        que: 'Lorem ipsum dolor sit amet is bad',
+        ans: [
+            {
+                from  : 'Staff',
+                replay: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.',
+            },
+        ],
+    },
+]
